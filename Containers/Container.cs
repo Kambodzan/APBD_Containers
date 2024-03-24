ed23f4e26bd8a4a3a4a006b6b37b7fe2708abb3c
Kambodzan <164697519+Kambodzan@users.noreply.github.com>
2024-03-24 19:33:08 +0100

    Containers1

--- Containers/Container.cs
+++ Containers/Container.cs
@@ -8,6 +8,29 @@ public abstract class Container
     public double depth { get; protected set; }
     public string serialNumber { get; protected set; }
     public double maxLoadMass { get; protected set; }
-    
-    
+
+    protected Container(double cargoMass, double height, double ownWeight, double depth, string serialNumber, double maxLoadMass)
+    {
+        this.cargoMass = cargoMass;
+        this.height = height;
+        this.ownWeight = ownWeight;
+        this.depth = depth;
+        this.serialNumber = serialNumber;
+        this.maxLoadMass = maxLoadMass;
+    }
+
+    public void loadCargo(double loadMass)
+    {
+        if (loadMass > maxLoadMass)
+        {
+            // TODO Add overfill exception
+        }
+
+        cargoMass = loadMass;
+    }
+
+    public void unloadCargo()
+    {
+        cargoMass = 0;
+    }
 }
---
+++ Containers/LiquidContainer.cs
@@ -0,0 +1,11 @@
+﻿namespace Containers;
+
+public class LiquidContainer : Container
+{
+    public bool isHazardous { get; set; }
+
+    public LiquidContainer(double cargoMass, double height, double ownWeight, double depth, string serialNumber, double maxLoadMass, bool isHazardous) : base(cargoMass, height, ownWeight, depth, serialNumber, maxLoadMass)
+    {
+        this.isHazardous = isHazardous;
+    }
+}
