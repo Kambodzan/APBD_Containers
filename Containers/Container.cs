d3d517ac672137772fe8f247dadb66c24b7d2969
Kambodzan <164697519+Kambodzan@users.noreply.github.com>
2024-03-24 01:17:04 +0100

    Second Commit

---
+++ Containers/Container.cs
@@ -0,0 +1,13 @@
+﻿namespace Containers;
+
+public abstract class Container
+{
+    public double cargoMass { get; protected set; }
+    public double height { get; protected set; }
+    public double ownWeight { get; protected set; }
+    public double depth { get; protected set; }
+    public string serialNumber { get; protected set; }
+    public double maxLoadMass { get; protected set; }
+    
+    
+}
